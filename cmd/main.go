8ce13de2881e37dfed37023c992d17254cecad5d
cs <watchmaker3896@gmail.com>
2023-08-13 22:11:08 -0500

    latest tests

--- cmd/main.go
+++ cmd/main.go
@@ -19,14 +19,17 @@ func main() {
 
 	Must("turn on", bolt.PowerOn())
 	time.Sleep(1 * time.Second)
-	//Must("turn on grid", bolt.LightUpGrid())
-	//time.Sleep(1 * time.Second)
-	Must("roll", bolt.Roll())
-	time.Sleep(1 * time.Second)
-	Must("roll", bolt.Roll())
-	time.Sleep(1 * time.Second)
-	//Must("stop rolling", bolt.StopRoll())
-	//time.Sleep(1 * time.Second)
+
+	// roll along square path (rhomboid in practice)
+	for i := 0; i < 4; i++ {
+		Must("set direction", bolt.SetDirection(i*90))
+
+		Must("start moving", bolt.SetSpeed(20))
+		time.Sleep(1 * time.Second)
+
+		Must("stop moving", bolt.SetSpeed(0))
+	}
+
 	Must("turn off", bolt.PowerOff())
 	time.Sleep(1 * time.Second)
 }
